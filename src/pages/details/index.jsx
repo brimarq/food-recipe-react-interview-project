3d0b874ad0589e2b30b28589d26318a17b943438
Brian Marquis <rbmarquis.dev@gmail.com>
2024-04-08 21:49:18 -0500

    add ingredients details

--- src/pages/details/index.jsx
+++ src/pages/details/index.jsx
@@ -34,6 +34,36 @@ export default function Details() {
           />
         </div>
       </div>
+      <div className="flex flex-col gap-3">
+        <span className="text-sm text-cyan-700 font-medium">
+          {recipeDetailsData?.recipe?.publisher}
+        </span>
+        <h3 className="font-bold text-2xl truncate text-black">
+          {recipeDetailsData?.recipe?.title}
+        </h3>
+        <div>
+          <button className="p-3 px-8 rounded-lg text-sm uppercase font-medium tracking-wider mt-3 inline-block shadow-md bg-black text-white">
+            Save to favorites
+          </button>
+        </div>
+        <div>
+          <span className="text-2xl font-semibold text-black">
+            Ingredients:
+          </span>
+          <ul className="flex flex-col gap-3 ">
+            {recipeDetailsData?.recipe?.ingredients.map((ingredient, i) => (
+              <li key={i}>
+                <span className="text-2xl font-semibold text-black">
+                  {ingredient.quantity} {ingredient.unit}
+                </span>
+                <span className="text-2xl font-semibold text-black">
+                  &nbsp;{ingredient.description}
+                </span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
